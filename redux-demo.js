dee9fadaff64e670b94101afd8c5c32aecbacf4c
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-22 04:24:23 -0700

    update files;

--- redux-demo.js
+++ redux-demo.js
@@ -1,17 +1,28 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const counterReducer = (state = { counter: 0},action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
     return {
-        counter: state.counter + 1,
+      counter: state.counter + 1,
     };
-
-}
+  }
+   if (action.type === 'decrement') {
+    return {
+       counter: state.counter - 1
+    }
+  }
+  return state;
+};
 
 const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState);
+  const latestState = store.getState();
+  console.log(latestState);
 };
 
 store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({type: 'decrement'});
+store.dispatch({type: 'decrement'});
