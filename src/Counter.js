f5eeac6a4f1d47a652fe11e0f821409786c7de90
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-22 08:33:57 -0700

    add files

--- src/Counter.js
+++ src/Counter.js
@@ -8,6 +8,9 @@ const Counter = () => {
     const incrementHandler = () => {
         dispatch({type: 'increment'});
     }
+    const increaseHandler = () => {
+        dispatch({type: 'increase', amount: 5})
+    }
     const decrementHandler = () => {
         dispatch({type: 'decrement'});
     }
@@ -21,6 +24,7 @@ const Counter = () => {
         <div>
             <button onClick={incrementHandler}>Increment</button>
             <button onClick={decrementHandler}>Decrement</button>
+            <button onClick={increaseHandler}>Increase by 5</button>
         </div>
         <button onClick = {toggleCounterHandler()}>Toggle Counter</button>
         </main>
