1038efd7dcb15f13ddbc268247a36e70df2d6e86
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-22 04:37:25 -0700

    modify files

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
+ <React.StrictMode>
     <App />
-  </Provider>
+
+ </React.StrictMode>
+  
+
+ 
   
 );
 
